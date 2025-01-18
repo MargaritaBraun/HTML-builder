4318071eca3867eab2cfce3571c7e4e624a4728d
Margarita Braun <margobraun777@gmail.com>
2025-01-18 18:57:17 +0300

    feat: 02-write-file is done

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const path = require('node:path');
+const fs = require('node:fs');
+// stderr,
+const { stdout, stdin } = process;
+
+const readline = require('node:readline').createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+const localPath = path.join(__dirname, 'text.txt');
+
+// const writeStream = fs.createWriteStream(localPath, { flags: 'a' });
+
+// function writeText(text) {
+//   writeStream.write(`${text}\n`);
+//   readline.prompt();
+// }
+// writeStream.write('Hello, World!\n');
+readline.setPrompt('Please enter text: ');
+readline.prompt();
+readline.on('line', (input) => {
+  if (input.trim().toLowerCase() === '.exit') {
+    readline.close();
+  } else {
+    fs.appendFileSync(localPath, `${input}\n`);
+  }
+});
+
+readline.on('close', () => {
+  //   writeStream.end();
+  stdout.write('Goodbye!\n');
+});
